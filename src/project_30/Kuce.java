b79904dc1abef88c76ec90a343ef9c3bcbb119fc
bojangjorgjievski4 <bojangjorgjievski399@gmail.com>
2023-12-30 08:03:45 +0100

    firstcommit

---
+++ src/project_30/Kuce.java
@@ -0,0 +1,37 @@
+package project_30;
+
+public class Kuce {
+
+	  public void osobina () {
+
+		  System.out.println("Kuceto lae");
+
+	  }
+
+	}
+
+	class LoveckoKuce extends Kuce {
+
+		public void osobina () {
+
+			System.out.println ("Loveckoto Kuce dusku");
+
+		}
+
+	}
+
+	class glavna {
+
+		public static void main (String []args) {
+
+			Kuce reks = new Kuce ();
+
+			reks.osobina ();
+
+			LoveckoKuce lovec=new
+
+					lovec.osobina ();
+
+		}
+
+		}
